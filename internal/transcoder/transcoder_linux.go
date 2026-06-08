f46bc7a542cd6bf23081e0d40dc94910ddb92299
Christophe Lambin <christophe.lambin@gmail.com>
2026-06-08 23:26:52 +0200

    fix linux build

--- internal/transcoder/transcoder_linux.go
+++ internal/transcoder/transcoder_linux.go
@@ -1,5 +1,12 @@
 package transcoder
 
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/clambin/xcoder/ffmpeg"
+)
+
 func DecoderArguments(videoStats ffmpeg.VideoStats) []string {
 	switch videoStats.VideoCodec {
 	case "h264", "hevc":
@@ -9,7 +16,7 @@ func DecoderArguments(videoStats ffmpeg.VideoStats) []string {
 	}
 }
 
-func encoderArguments(videoStats ffmpeg.VideoStats) []string {
+func encoderArguments(videoStats ffmpeg.VideoStats) ([]string, error) {
 	switch videoStats.VideoCodec {
 	case "hevc":
 		profileName := "main"
@@ -22,8 +29,8 @@ func encoderArguments(videoStats ffmpeg.VideoStats) []string {
 			"-profile:v", profileName,
 			"-c:a", "copy",
 			"-c:s", "copy",
-		}
+		}, nil
 	default:
-		return nil
+		return nil, fmt.Errorf("unsupported codec: %s", videoStats.VideoCodec)
 	}
 }
